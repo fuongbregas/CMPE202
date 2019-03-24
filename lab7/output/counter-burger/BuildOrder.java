0a23dc4815b8915e924f5b513c686d888b9ea4b4
fuongbregas <fuongbregas@icloud.com>
2019-03-23 18:57:38 -0700

    finish BuildOrder

--- lab7/output/counter-burger/BuildOrder.java
+++ lab7/output/counter-burger/BuildOrder.java
@@ -5,8 +5,7 @@ public class BuildOrder {
     public static Component getOrder()
     {
         Composite order = new Composite( "Order" ) ;
-        order.addChild(new Leaf("Crispy Onion Strings", 5.50 ));
-        order.addChild(new Leaf("The Purist", 8.00 ));
+        
 
         CustomBurger customBurger1 = new CustomBurger( "Build Your Own Burger" ) ;
         // base price for 1/3 lb
@@ -65,8 +64,71 @@ public class BuildOrder {
         customBurger1.addChild(bun1);
         customBurger1.addChild(si1);
         
+        
+        // Burger 2
+        CustomBurger customBurger2 = new CustomBurger( "Build Your Own Burger" ) ;
+        // base price for 1/3 lb
+        Burger b2 = new Burger( "Burger Options" ) ;
+        String[] bo2 = { "Hormone & Antibiotic Free Beef*", 
+        		"1/3lb.", "On A Bun" } ;
+        b2.setOptions( bo2 ) ;
+        
+        // 1 cheese free, extra cheese +1.00
+        Cheese c2 = new Cheese( "Cheese Options" ) ;
+        String[] co2 = { "Smoked Gouda", "Greek Feta"} ;
+        c2.setOptions( co2 ) ;
+        c2.wrapDecorator( b2 ) ;
+        
+     // Premium Cheese
+        PremiumCheese pc2 = new PremiumCheese( "Premium Cheese Options" ) ;
+        String[] pco2 = { "Fresh Mozzarella" } ;
+        pc2.setOptions( pco2 ) ;
+        pc2.wrapDecorator( c2 ) ;
+        
+     // 1 sauce free, extra +.5
+        Sauce s2 = new Sauce( "Sauce Options" ) ;
+        String[] so2 = { "Habanero Salsa" } ;
+        s2.setOptions( so2 ) ;
+        s2.wrapDecorator( pc2 ) ;
+        
+        Toppings t2 = new Toppings("Toppings Options");
+        String[] to2 = { "Crushed Peanuts" };
+        t2.setOptions(to2);
+        t2.wrapDecorator(s2);
+        
+        // Premium Toppings
+        PremiumToppings pt2 = new PremiumToppings( "Premium Toppings Options" ) ;
+        String[] pto2 = { "Sunny Side Up Egg*",  "Marinated Tomatoes"} ;
+        pt2.setOptions( pto2 ) ;
+        pt2.wrapDecorator(t2); 
+        
+        // Bun
+        Bun bun2 = new Bun("Bun Options");
+        String[] bun2o =  { "Ciabatta (Vegan)"  };
+        bun2.setOptions(bun2o);
+        bun2.wrapDecorator(pt2);
+        
+        // Side
+        Side si2 = new Side("Side Options");
+        String[] sio2 = {"Shoestring Fries"};
+        si2.setOptions(sio2);
+        si2.wrapDecorator(bun2);
+        
+        // Setup Custom Burger Ingredients
+        customBurger2.setDecorator( s2 ) ;
+        customBurger2.addChild( b2 ) ;
+        customBurger2.addChild( c2 ) ;
+        customBurger2.addChild( pc2 ) ;
+        customBurger2.addChild( s2 ) ;
+        customBurger2.addChild( t2 ) ;
+        customBurger2.addChild(pt2);
+        customBurger2.addChild(bun2);
+        customBurger2.addChild(si2);
+        
+        
         // Add Custom Burger to the ORder
         order.addChild( customBurger1 );
+        order.addChild( customBurger2 );
         return order ;
     }
 
