113c35deff2ca74b4f3c68bb3dc1d7f5b72f93cf
fuongbregas <fuongbregas@icloud.com>
2019-04-04 23:35:51 -0700

    CVC

--- lab8/output/inputmask/src/main/java/CreditCardCVC.java
+++ lab8/output/inputmask/src/main/java/CreditCardCVC.java
@@ -18,9 +18,16 @@ public class CreditCardCVC implements IDisplayComponent, IKeyEventHandler
 	}	
 
 	public void key(String ch, int cnt) {
+		String checker = ch.toLowerCase();
 		if ( cnt >= 21 ) {
 			if ( cnt >= 21 && cnt <= 23 )
-				cvc += ch ;
+				if(checker.equals("x") && ch.length() > 0) {
+					cvc = cvc.substring(0, cvc.length() - 1);
+				}
+				else {
+					cvc += ch ;
+				}
+				
 			else if ( nextHandler != null )
 				nextHandler.key(ch, cnt) ;
 		}
